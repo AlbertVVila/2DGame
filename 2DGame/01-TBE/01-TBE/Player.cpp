e6994b906daf41791ee4f1a1ea101e1ca0e984fd
AlbertVVila <albert.val.vila@est.fib.upc.edu>
2017-03-18 17:20:03 +0100

    shaders + update animacions

--- 2DGame/01-TBE/01-TBE/Player.cpp
+++ 2DGame/01-TBE/01-TBE/Player.cpp
@@ -13,18 +13,19 @@
 
 enum PlayerAnims
 {
-	STAND_LEFT, STAND_RIGHT, STANDLR, STANDRL, MOVE_LEFT, MOVE_RIGHT
+	STAND_LEFT, STAND_RIGHT, STANDLR, STANDRL,START_LEFT, START_RIGHT,MOVE_LEFT, MOVE_RIGHT, STOP_LEFT , STOP_RIGHT
 };
 
 
 void Player::init(const glm::ivec2 &tileMapPos, ShaderProgram &shaderProgram)
 {
 	bJumping = false;
+	bRunning = false;
 	direction = "left";
 	spritesheet.setWrapS(GL_MIRRORED_REPEAT);
 	spritesheet.loadFromFile("images/kidrun.png", TEXTURE_PIXEL_FORMAT_RGBA);
 	sprite = Sprite::createSprite(glm::ivec2(64, 64), glm::vec2(0.1, 0.05), &spritesheet, &shaderProgram);
-	sprite->setNumberAnimations(6);
+	sprite->setNumberAnimations(10);
 
 	sprite->setAnimationSpeed(STAND_LEFT, 8);
 	sprite->addKeyframe(STAND_LEFT, glm::vec2(0.f, 0.f));
@@ -52,11 +53,19 @@ void Player::init(const glm::ivec2 &tileMapPos, ShaderProgram &shaderProgram)
 	sprite->addKeyframe(STANDRL, glm::vec2(0.1f, 0.1f));
 	sprite->addKeyframe(STANDRL, glm::vec2(0.2f, 0.1f));
 
+	sprite->setAnimationSpeed(START_LEFT, 8);
+	sprite->addKeyframe(START_LEFT, glm::vec2(0.1f, 0.f));
+	sprite->addKeyframe(START_LEFT, glm::vec2(0.2f, 0.f));
+	sprite->addKeyframe(START_LEFT, glm::vec2(0.3f, 0.f));
+	sprite->addKeyframe(START_LEFT, glm::vec2(0.4f, 0.f));
+
+	sprite->setAnimationSpeed(START_RIGHT, 8);
+	sprite->addKeyframe(START_RIGHT, glm::vec2(-0.2f, 0.f));
+	sprite->addKeyframe(START_RIGHT, glm::vec2(-0.3f, 0.f));
+	sprite->addKeyframe(START_RIGHT, glm::vec2(-0.4f, 0.f));
+	sprite->addKeyframe(START_RIGHT, glm::vec2(-0.5f, 0.f));
+
 	sprite->setAnimationSpeed(MOVE_LEFT, 8);
-	sprite->addKeyframe(MOVE_LEFT, glm::vec2(0.1f, 0.f));
-	sprite->addKeyframe(MOVE_LEFT, glm::vec2(0.2f, 0.f));
-	sprite->addKeyframe(MOVE_LEFT, glm::vec2(0.3f, 0.f));
-	sprite->addKeyframe(MOVE_LEFT, glm::vec2(0.4f, 0.f));
 	sprite->addKeyframe(MOVE_LEFT, glm::vec2(0.5f, 0.f));
 	sprite->addKeyframe(MOVE_LEFT, glm::vec2(0.6f, 0.f));
 	sprite->addKeyframe(MOVE_LEFT, glm::vec2(0.7f, 0.f));
@@ -69,10 +78,6 @@ void Player::init(const glm::ivec2 &tileMapPos, ShaderProgram &shaderProgram)
 	sprite->addKeyframe(MOVE_LEFT, glm::vec2(0.4f, 0.05f));
 
 	sprite->setAnimationSpeed(MOVE_RIGHT, 8);
-	sprite->addKeyframe(MOVE_RIGHT, glm::vec2(-0.2f, 0.f));
-	sprite->addKeyframe(MOVE_RIGHT, glm::vec2(-0.3f, 0.f));
-	sprite->addKeyframe(MOVE_RIGHT, glm::vec2(-0.4f, 0.f));
-	sprite->addKeyframe(MOVE_RIGHT, glm::vec2(-0.5f, 0.f));
 	sprite->addKeyframe(MOVE_RIGHT, glm::vec2(-0.6f, 0.f));
 	sprite->addKeyframe(MOVE_RIGHT, glm::vec2(-0.7f, 0.f));
 	sprite->addKeyframe(MOVE_RIGHT, glm::vec2(-0.8f, 0.f));
@@ -84,6 +89,19 @@ void Player::init(const glm::ivec2 &tileMapPos, ShaderProgram &shaderProgram)
 	sprite->addKeyframe(MOVE_RIGHT, glm::vec2(-0.4f, 0.05f));
 	sprite->addKeyframe(MOVE_RIGHT, glm::vec2(-0.5f, 0.05f));
 
+
+	sprite->setAnimationSpeed(STOP_LEFT, 8);
+	sprite->addKeyframe(STOP_LEFT, glm::vec2(0.3f, 0.1f));
+	sprite->addKeyframe(STOP_LEFT, glm::vec2(0.4f, 0.1f));
+	sprite->addKeyframe(STOP_LEFT, glm::vec2(0.5f, 0.1f));
+	sprite->addKeyframe(STOP_LEFT, glm::vec2(0.6f, 0.1f));
+
+	sprite->setAnimationSpeed(STOP_RIGHT, 8);
+	sprite->addKeyframe(STOP_RIGHT, glm::vec2(-0.4f, 0.1f));
+	sprite->addKeyframe(STOP_RIGHT, glm::vec2(-0.5f, 0.1f));
+	sprite->addKeyframe(STOP_RIGHT, glm::vec2(-0.6f, 0.1f));
+	sprite->addKeyframe(STOP_RIGHT, glm::vec2(-0.7f, 0.1f));
+
 	sprite->changeAnimation(0);
 	tileMapDispl = tileMapPos;
 	sprite->setPosition(glm::vec2(float(tileMapDispl.x+posPlayer.x), float(tileMapDispl.y+posPlayer.y)));
@@ -93,15 +111,40 @@ void Player::init(const glm::ivec2 &tileMapPos, ShaderProgram &shaderProgram)
 void Player::update(int deltaTime)
 {
 	sprite->update(deltaTime);
+	switch (sprite->animation()){
+	case MOVE_LEFT:
+		posPlayer.x -= 2;
+		break;
+	case MOVE_RIGHT:
+		posPlayer.x += 2;
+		break;
+	case STOP_LEFT:
+		posPlayer.x -= 2;
+		break;
+	case STOP_RIGHT:
+		posPlayer.x += 2;
+		break;
+	case START_LEFT:
+		posPlayer.x -= 2;
+		break;
+	case START_RIGHT:
+		posPlayer.x += 2;
+		break;
+	}
 	if (Game::instance().getSpecialKey(GLUT_KEY_LEFT))
 	{
-		if (direction != "left" && sprite->animFinished()){
+		if (direction != "left" && sprite->animFinished() && !bRunning){
 			sprite->changeAnimation(STANDRL);
 			direction = "left";
 		}
-		else if (sprite->animation() != MOVE_LEFT && sprite->animFinished())
+		else if (sprite->animation() != MOVE_LEFT && sprite->animFinished() && !bRunning){ //abans sempre si no mira en la bona direcció es gira
+			sprite->changeAnimation(START_LEFT);
+			bRunning = true;
+		}
+
+		else if (sprite->animation() == START_LEFT && sprite->animFinished())
 			sprite->changeAnimation(MOVE_LEFT);
-		posPlayer.x -= 2;
+
 		if (map->collisionMoveLeft(posPlayer, glm::ivec2(32, 64)))
 		{
 			posPlayer.x += 2;
@@ -110,34 +153,38 @@ void Player::update(int deltaTime)
 	}
 	else if (Game::instance().getSpecialKey(GLUT_KEY_RIGHT))
 	{
-		if (direction != "right" && sprite->animFinished()){
+		if (direction != "right" && sprite->animFinished() && !bRunning){
 			sprite->changeAnimation(STANDLR);
 			direction = "right";
 		}
-		else if (sprite->animation() != MOVE_RIGHT && sprite->animFinished())
+		else if (sprite->animation() != MOVE_RIGHT && sprite->animFinished() && !bRunning){
+			sprite->changeAnimation(START_RIGHT);
+			bRunning = true;
+		}
+
+		else if (sprite->animation() == START_RIGHT && sprite->animFinished())
 			sprite->changeAnimation(MOVE_RIGHT);
-			posPlayer.x += 2;
+
 			if (map->collisionMoveRight(posPlayer, glm::ivec2(32, 64)))
 			{
 				posPlayer.x -= 2;
 				sprite->changeAnimation(STAND_RIGHT);
 			}
-		
-		/*if (sprite->animation() != MOVE_RIGHT)
-			sprite->changeAnimation(MOVE_RIGHT);
-		posPlayer.x += 2;
-		if (map->collisionMoveRight(posPlayer, glm::ivec2(32, 64)))
-		{
-			posPlayer.x -= 2;
-			sprite->changeAnimation(STAND_RIGHT);
-		}*/
 	}
 	else if (sprite->animFinished()) // ni left ni right apretat
 	{
-		if (direction == "left")
+		if (sprite->animation() == STOP_LEFT ||  sprite->animation() == STANDRL)//substituir per not running
 			sprite->changeAnimation(STAND_LEFT);
-		else if (direction == "right")
+		if (sprite->animation() == STOP_RIGHT ||  sprite->animation() == STANDLR)
 			sprite->changeAnimation(STAND_RIGHT);
+		if (bRunning && direction == "left"){
+			sprite->changeAnimation(STOP_LEFT);
+			bRunning = false;
+		}
+		if (bRunning && direction == "right"){
+			sprite->changeAnimation(STOP_RIGHT);
+			bRunning = false;
+		}
 	}
 
 	if (bJumping)
--- 2DGame/01-TBE/01-TBE/Player.h
+++ 2DGame/01-TBE/01-TBE/Player.h
@@ -22,7 +22,7 @@ public:
 	void setPosition(const glm::vec2 &pos);
 
 private:
-	bool bJumping;
+	bool bJumping, bRunning;
 	string direction;
 	glm::ivec2 tileMapDispl, posPlayer;
 	int jumpAngle, startY;
